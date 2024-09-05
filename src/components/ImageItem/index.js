f4b9eda1574086a92203541e06adc4c61a8883e8
Sandeepnaga21 <147514083+Sandeepnaga21@users.noreply.github.com>
2024-09-05 18:23:08 +0530

    Update index.js

--- src/components/ImageItem/index.js
+++ src/components/ImageItem/index.js
@@ -2,18 +2,18 @@ import './index.css'
 
 const ImageItem = props => {
   const {imageDetails, updateImage, updateScore} = props
-  const {id, imageUrl, category} = imageDetails
+  const {id, category, thumbnailUrl} = imageDetails
 
   const onClickImg = () => {
     updateImage(id)
-    updateScore(id)
+    updateScore(thumbnailUrl)
   }
 
   return (
     <div>
       <li key={category}>
         <button type="button" onClick={onClickImg}>
-          <img src={imageUrl} alt={category} value={id} />
+          <img src={thumbnailUrl} alt={category} value={id} />
         </button>
       </li>
     </div>
